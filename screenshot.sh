1efc0f1a9d7d55aeaf5baa4818697c3d91aa67a5
Your Name <you@example.com>
2026-01-24 11:35:46 -0700

    update

--- screenshot.sh
+++ screenshot.sh
@@ -8,21 +8,17 @@ FILENAME="$DIR/screenshot_$(date +'%Y-%m-%d_%H-%M-%S').png"
 CHOICE=$(printf "Full Screen\nSelect Area\nActive Window\nDelay 5s" | dmenu -i -p "Screenshot:")
 
 case "$CHOICE" in
-    "Full Screen")
-        scrot "$FILENAME" && notify-send "Screenshot saved" "$FILENAME" 
-        ;;
-    "Select Area")
+"Full Screen")
+	xnap -f >"$FILENAME" && notify-send "Screenshot saved" "$FILENAME"
+	;;
+"Select Area")
 	sleep 0.08
-	scrot -s -f "$FILENAME" && notify-send "Screenshot saved" "$FILENAME" 
-        ;;
-    "Active Window")
-        scrot -u "$FILENAME" && notify-send "Screenshot saved" "$FILENAME" 
-        ;;
-    "Delay 5s")
-        sleep 5 && scrot "$FILENAME" && notify-send "Screenshot saved" "$FILENAME" 
-        ;;
-    *)
-        exit 0
-        ;;
+	xnap >"$FILENAME" && notify-send "Screenshot saved" "$FILENAME"
+	;;
+"Delay 5s")
+	sleep 5 && xnap -f >"$FILENAME" && notify-send "Screenshot saved" "$FILENAME"
+	;;
+*)
+	exit 0
+	;;
 esac
-
