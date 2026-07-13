e1be2b8d3f66f4ed313c9d6d46ed5da2209a1375
Your Name <you@example.com>
2026-07-13 17:58:24 -0600

    update

--- screenshot.sh
+++ screenshot.sh
@@ -5,7 +5,7 @@ mkdir -p "$DIR"
 
 FILENAME="$DIR/screenshot_$(date +'%Y-%m-%d_%H-%M-%S')"
 
-CHOICE=$(printf "Full Screen\nSelect Area\nSelect QR Code\nSelect Area (5s delay)" | dmenu -i -p "Screenshot:")
+CHOICE=$(printf "Full Screen\nSelect Area\nSelect Area (5s delay)" | dmenu -i -p "Screenshot:")
 
 case "$CHOICE" in
 "Full Screen")
@@ -18,10 +18,6 @@ case "$CHOICE" in
 	sleep 0.08
 	xnap | convert ppm:- "${FILENAME}.png" && notify-send "Screenshot saved" "${FILENAME}.png"
 	xclip -selection clipboard -t image/png -i "${FILENAME}.png"
-	;;
-"Select QR Code")
-	sleep 0.08
-	xnap | zbarimg -q --raw - | xclip -selection clipboard && notify-send "Text copied to clipboard!"
 	;;
 "Select Area (5s delay)")
 	sleep 5 && xnap -f >"$FILENAME" && notify-send "Screenshot saved" "$FILENAME"
